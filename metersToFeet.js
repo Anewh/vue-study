3c31325adb7de5795484b2eefda74aac9b735c1c
Anewh <i0ianrusi0i@gmail.com>
2022-10-25 23:03:02 +0300

    complete firs taks

---
+++ metersToFeet.js
@@ -0,0 +1,38 @@
+// Для вычисляемого свойства get и set нужны из-за того, что используется v-model
+// v-model нужен для двунаправленного связывания данных с элементами форм input, textarea и select. 
+// Способ обновления элемента выбирается автоматически в зависимости от типа элемента. 
+// Начальное значение задается в data в js - это value
+var computedMetersFeet = new Vue({
+el: '#converterMetersFeet',
+data: {
+  value: 0,
+  multiplier: 3.28084,
+  isInitialValue: true,
+},
+computed: {
+    answer: {
+      get: function () {
+          return this.value*this.multiplier;
+        },
+      set: function (newValue){
+          this.answer = this.value;
+        }
+    },
+    reverseAnswer:{
+      get: function () {
+          return this.value/this.multiplier;
+        },
+      set: function (newValue){
+          this.answer = this.value;
+        }
+    },
+    typeOfConverter:{
+      get: function () {
+          return this.isInitialValue;
+        },
+      set: function (newValue){
+          this.isInitialValue = newValue;
+        }
+    }
+  }
+})
